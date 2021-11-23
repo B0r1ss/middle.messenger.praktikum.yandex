2a5d5495b7d9ab72439f39a26e055d8ef32b9bec
B0r1ss <borisenko.va90@gmail.com>
2021-11-23 12:12:05 +0300

    ref aft rev

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import "./index.scss";
+import Login from "./pages/Login/Login";
 import MainPage from "./pages/MainPage/MainPage";
 import NotFound from "./pages/NotFound/NotFound";
 import Profile from "./pages/Profile/Profile.js";
 import Register from "./pages/Register/Register";
+import Error from "./pages/Error/Error";
+import NotFound from "./pages/NotFound/NotFound";
 
 
 const root = document.querySelector("#root");
 
 const path = window.location.pathname;
-console.log(path)
 
 switch (path) {
     case "/authorize":
-        root.innerHTML = authorize();
+        root.innerHTML = Login();
         break;
     case "/profile":
         root.innerHTML = Profile();
@@ -24,7 +26,7 @@ switch (path) {
         root.innerHTML = MainPage();
         break;
     case "/500":
-        root.innerHTML = error({code: "500", message: "Мы уже фиксим"});
+        root.innerHTML = Error({code: "500", message: "Мы уже фиксим"});
         break;
     default:
         root.innerHTML = NotFound();
